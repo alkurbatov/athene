80149abb779de77ce74c79a646de535e599b1065
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2018-08-25 21:04:06 +0300

    collect_minerals_and_gas: Improve action space
    * Reduce action space by removing the 'select' actions.
    * Apply stages to the actions as actions takes several iterations to
    execute. Now the agent learns only after the end of the last stage.

--- athene/collect_minerals_and_gas/agent.py
+++ athene/collect_minerals_and_gas/agent.py
@@ -19,8 +19,6 @@ from athene.api.actions import \
     ACTION_BUILD_SUPPLY, \
     ACTION_DO_NOTHING, \
     ACTION_HARVEST_MINERALS, \
-    ACTION_SELECT_COMMAND_CENTER, \
-    ACTION_SELECT_IDLE_WORKER, \
     ACTION_TRAIN_SCV
 from athene.api.screen import UnitPos, UnitPosList
 from athene.brain.qlearning import QLearningTable
@@ -35,9 +33,7 @@ class Agent(base_agent.BaseAgent):
 
         self.smart_actions = [
             ACTION_DO_NOTHING,
-            ACTION_SELECT_IDLE_WORKER,
             ACTION_HARVEST_MINERALS,
-            ACTION_SELECT_COMMAND_CENTER,
             ACTION_TRAIN_SCV,
             ACTION_BUILD_SUPPLY,
             ACTION_BUILD_REFINERY,
@@ -48,8 +44,13 @@ class Agent(base_agent.BaseAgent):
             src=self.DATA_FOLDER,
         )
 
+        # NOTE (alkurbatov): Many tasks are done in to steps (stages):
+        # 1 - Select a unit.
+        # 2 - Issue an order.
+        self.stage = 0
+
         self.town_hall = None
-        self.previous_action = None
+        self.executed_action = None
         self.previous_state = None
 
     def step(self, obs):
@@ -58,13 +59,14 @@ class Agent(base_agent.BaseAgent):
         unit_type = obs.observation.feature_screen.unit_type
 
         if obs.first():
+            self.stage = 1
             cc_y, cc_x = (unit_type == units.Terran.CommandCenter).nonzero()
             self.town_hall = UnitPos(cc_x, cc_y)
 
         if obs.last():
             self.qlearn.learn(
                 self.previous_state,
-                self.previous_action,
+                self.executed_action,
                 obs.reward,
                 'terminal'
             )
@@ -73,109 +75,114 @@ class Agent(base_agent.BaseAgent):
             return actions.FUNCTIONS.no_op()
 
         supplies = UnitPosList.locate(obs, units.Terran.SupplyDepot)
-        refineries = UnitPosList.locate(obs, units.Terran.Refinery)
 
-        current_state = (
-            obs.observation.player.idle_worker_count,
-            obs.observation.player.food_workers,
-            len(supplies),
-            len(refineries)
-        )
+        if self.stage == 1:
+            self.stage += 1
 
-        excluded_actions = set()
+            refineries = UnitPosList.locate(obs, units.Terran.Refinery)
 
-        if obs.observation.player.idle_worker_count == 0:
-            excluded_actions.add(ACTION_SELECT_IDLE_WORKER)
-            excluded_actions.add(ACTION_HARVEST_MINERALS)
-
-        if obs.observation.player.food_cap == obs.observation.player.food_used:
-            excluded_actions.add(ACTION_SELECT_COMMAND_CENTER)
-            excluded_actions.add(ACTION_TRAIN_SCV)
-
-        if len(supplies) >= 2:
-            excluded_actions.add(ACTION_BUILD_SUPPLY)
-
-        if len(refineries) >= 4:
-            excluded_actions.add(ACTION_BUILD_REFINERY)
-
-        if self.previous_action:
-            self.qlearn.learn(
-                self.previous_state,
-                self.previous_action,
-                0,
-                current_state
+            current_state = (
+                obs.observation.player.idle_worker_count,
+                obs.observation.player.food_workers,
+                len(supplies),
+                len(refineries)
             )
 
-        smart_action = self.qlearn.choose_action(
-            current_state,
-            excluded_actions=excluded_actions
-        )
+            excluded_actions = set()
 
-        self.previous_state = current_state
-        self.previous_action = smart_action
+            if obs.observation.player.idle_worker_count == 0:
+                excluded_actions.add(ACTION_HARVEST_MINERALS)
 
-        if smart_action == ACTION_SELECT_COMMAND_CENTER:
-            cc_y, cc_x = (unit_type == units.Terran.CommandCenter).nonzero()
+            if obs.observation.player.food_cap == obs.observation.player.food_used:
+                excluded_actions.add(ACTION_TRAIN_SCV)
 
-            if not cc_y.any():
-                print('[WARNING] Where is your Command Center?')
+            if len(supplies) >= 2:
+                excluded_actions.add(ACTION_BUILD_SUPPLY)
+
+            if len(refineries) >= 4:
+                excluded_actions.add(ACTION_BUILD_REFINERY)
+
+            if self.executed_action:
+                self.qlearn.learn(
+                    self.previous_state,
+                    self.executed_action,
+                    0,
+                    current_state
+                )
+
+            smart_action = self.qlearn.choose_action(
+                current_state,
+                excluded_actions=excluded_actions
+            )
+
+            self.previous_state = current_state
+            self.executed_action = smart_action
+
+            if smart_action == ACTION_DO_NOTHING:
                 return actions.FUNCTIONS.no_op()
 
-            # NOTE (alkurbatov): There is only one command center on the screen.
-            cc = UnitPos(cc_x, cc_y)
-            return actions.FUNCTIONS.select_point('select', cc.center)
+            if smart_action == ACTION_TRAIN_SCV:
+                cc_y, cc_x = (unit_type == units.Terran.CommandCenter).nonzero()
 
-        if smart_action == ACTION_TRAIN_SCV:
-            if actions.FUNCTIONS.Train_SCV_quick.id not in obs.observation.available_actions:
-                return actions.FUNCTIONS.no_op()
+                if not cc_y.any():
+                    print('[WARNING] Where is your Command Center?')
+                    return actions.FUNCTIONS.no_op()
 
-            return actions.FUNCTIONS.Train_SCV_quick('now')
+                # NOTE (alkurbatov): There is only one command center on the screen.
+                cc = UnitPos(cc_x, cc_y)
+                return actions.FUNCTIONS.select_point('select', cc.center)
 
-        if smart_action == ACTION_SELECT_IDLE_WORKER:
+            # NOTE (alkurbatov): All other actions require an SCV.
             if actions.FUNCTIONS.select_idle_worker.id not in obs.observation.available_actions:
                 return actions.FUNCTIONS.no_op()
 
             return actions.FUNCTIONS.select_idle_worker('select')
 
-        if smart_action == ACTION_HARVEST_MINERALS:
-            if actions.FUNCTIONS.Harvest_Gather_screen.id not in obs.observation.available_actions:
-                return actions.FUNCTIONS.no_op()
+        if self.stage == 2:
+            self.stage = 1
 
-            minerals = UnitPosList.locate(obs, units.Neutral.MineralField)
+            if self.executed_action == ACTION_TRAIN_SCV:
+                if actions.FUNCTIONS.Train_SCV_quick.id not in obs.observation.available_actions:
+                    return actions.FUNCTIONS.no_op()
 
-            if not minerals:
-                print('[WARNING] No minerals?')
-                return actions.FUNCTIONS.no_op()
+                return actions.FUNCTIONS.Train_SCV_quick('now')
 
-            mineral_x, mineral_y = minerals.random_point()
-            mineral_patch = UnitPos(mineral_x, mineral_y)
+            if self.executed_action == ACTION_HARVEST_MINERALS:
+                minerals = UnitPosList.locate(obs, units.Neutral.MineralField)
 
-            return actions.FUNCTIONS.Harvest_Gather_screen('now', mineral_patch.pos)
+                if not minerals:
+                    print('[WARNING] No minerals?')
+                    return actions.FUNCTIONS.no_op()
 
-        if smart_action == ACTION_BUILD_REFINERY:
-            if actions.FUNCTIONS.Build_Refinery_screen.id not in obs.observation.available_actions:
-                return actions.FUNCTIONS.no_op()
+                mineral_x, mineral_y = minerals.random_point()
+                mineral_patch = UnitPos(mineral_x, mineral_y)
 
-            geysers = UnitPosList.locate(obs, units.Neutral.VespeneGeyser)
+                return actions.FUNCTIONS.Harvest_Gather_screen('now', mineral_patch.pos)
 
-            if not geysers:
-                print('[WARNING] No geysers?')
-                return actions.FUNCTIONS.no_op()
+            if self.executed_action == ACTION_BUILD_REFINERY:
+                if actions.FUNCTIONS.Build_Refinery_screen.id not in obs.observation.available_actions:
+                    return actions.FUNCTIONS.no_op()
 
-            geyser_x, geyser_y = geysers.random_point()
-            geyser = UnitPos(geyser_x, geyser_y)
+                geysers = UnitPosList.locate(obs, units.Neutral.VespeneGeyser)
 
-            return actions.FUNCTIONS.Build_Refinery_screen('now', geyser.pos)
+                if not geysers:
+                    print('[WARNING] No geysers?')
+                    return actions.FUNCTIONS.no_op()
 
-        if smart_action == ACTION_BUILD_SUPPLY:
-            if actions.FUNCTIONS.Build_SupplyDepot_screen.id not in obs.observation.available_actions:
-                return actions.FUNCTIONS.no_op()
+                geyser_x, geyser_y = geysers.random_point()
+                geyser = UnitPos(geyser_x, geyser_y)
 
-            if supplies:
+                return actions.FUNCTIONS.Build_Refinery_screen('now', geyser.pos)
+
+            if self.executed_action == ACTION_BUILD_SUPPLY:
+                if actions.FUNCTIONS.Build_SupplyDepot_screen.id not in obs.observation.available_actions:
+                    return actions.FUNCTIONS.no_op()
+
+                if supplies:
+                    return actions.FUNCTIONS.Build_SupplyDepot_screen(
+                        'now', self.town_hall.shift(0, -20).pos)
+
                 return actions.FUNCTIONS.Build_SupplyDepot_screen(
-                    'now', self.town_hall.shift(0, -20).pos)
-
-            return actions.FUNCTIONS.Build_SupplyDepot_screen(
-                'now', self.town_hall.shift(0, 20).pos)
+                    'now', self.town_hall.shift(0, 20).pos)
 
         return actions.FUNCTIONS.no_op()
